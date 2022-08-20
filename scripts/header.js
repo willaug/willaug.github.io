e5c5dc52adf3600d25c168b7c79811ab245b93bb
William Augusto <william.santos315@outlook.com>
2022-08-20 19:34:18 -0300

    feat: header and navbar

---
+++ scripts/header.js
@@ -0,0 +1,20 @@
+function toggleHeaderList() {
+  const headerListElement = document.querySelector('.header__list');
+  const headerListIsOpened = !!document.querySelector('.header__list--opened');
+
+  if (headerListIsOpened) {
+    removeOpenedClassFromHeaderList();
+    return;
+  };
+  
+  headerListElement.classList.add('header__list--opened');
+}
+
+function removeOpenedClassFromHeaderList() {
+  const headerListElement = document.querySelector('.header__list');
+  const headerListIsOpened = !!document.querySelector('.header__list--opened');
+  
+  if (headerListIsOpened) {
+    headerListElement.classList.remove('header__list--opened')
+  }
+}
