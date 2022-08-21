c0bd968b00bf9b5a23402dfd7175163243543873
William Augusto <william.santos315@outlook.com>
2022-08-21 15:48:50 -0300

    feat: my services

--- scripts/i18n/english.js
+++ scripts/i18n/english.js
@@ -7,6 +7,22 @@ const EN_INTERNATIONALIZATION = {
   },
   PRESENTATION: {
     TITLE: 'Hi, I\'m William',
-    TEXT: 'I\'m a full-stack developer and a clean code enthusiast. Through my skills, I seek to contribute with people and companies to achieve the expected goals. I\'ve been working professionally for over a year and I\'ve been a programming lover since I was fifteen.'
+    TEXT: 'I\'m a full-stack developer and a clean code enthusiast. Through my skills, I seek to contribute with people and companies to achieve the expected goals. I\'ve been working professionally for over a year and I\'ve been a programming lover since I was fifteen.',
+    BUTTON: 'Let\'s chat'
+  },
+  ABOUT: {
+    TITLE: 'What I do',
+    API: {
+      TITLE: 'APIs',
+      DESCRIPTION: 'Through APIs, we can create platforms that communicate with other platforms.'
+    },
+    SPA: {
+      TITLE: 'Web apps',
+      DESCRIPTION: 'With web applications, we can create platforms that behave similarly to an application.'
+    },
+    PWA: {
+      TITLE: 'Hybrids apps',
+      DESCRIPTION: 'Have you ever thought about running an application on several mobile devices, whether iPhone or Android? With a hybrid application it is possible.'
+    }
   }
 } 
--- scripts/i18n/portuguese.js
+++ scripts/i18n/portuguese.js
@@ -7,6 +7,22 @@ const PT_INTERNATIONALIZATION = {
   },
   PRESENTATION: {
     TITLE: 'Olá, eu sou o William',
-    TEXT: 'Eu sou um desenvolvedor full-stack e um entusiasta do código limpo. Com minhas habilidades, busco contribuir com pessoas e empresas para atingir seus objetivos. Possuo, profissionalmente, mais de um ano de experiência e desde os quinze anos sou um "programming lover".'
+    TEXT: 'Eu sou um desenvolvedor full-stack e um entusiasta do código limpo. Com minhas habilidades, busco contribuir com pessoas e empresas para atingir seus objetivos. Possuo, profissionalmente, mais de um ano de experiência e desde os quinze anos sou um "programming lover".',
+    BUTTON: 'Bora trocar uma ideia?'
+  },
+  ABOUT: {
+    TITLE: 'O que faço',
+    API: {
+      TITLE: 'APIs',
+      DESCRIPTION: 'Por meio de APIs, nós podemos criar plataformas que se comunicam com outras plataformas.'
+    },
+    SPA: {
+      TITLE: 'Aplicações web',
+      DESCRIPTION: 'Com aplicações web, podemos criar plataformas que se comportam de forma semelhante a um aplicativo.'
+    },
+    PWA: {
+      TITLE: 'Aplicações híbridas',
+      DESCRIPTION: 'Já pensou em rodar uma aplicação em diversos dispositivos móveis, seja Iphone ou Android? Com uma aplicação híbrida é possível.'
+    }
   }
 }
